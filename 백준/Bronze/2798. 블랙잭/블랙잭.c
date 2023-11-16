2874f3a51ca8aaf16ccfcf1a7aa40ff1cee95a33
solmee <87332713+ssolfa@users.noreply.github.com>
2023-11-16 14:45:17 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  int n, m, arr[100]={0,}, max=0, sum;  
+  scanf("%d %d", &n, &m);
+  for(int i=0 ; i<n ; i++){
+    scanf("%d", &arr[i]);
+  }
+  for(int i=0 ; i<n ; i++){
+    for(int j= i+1 ; j<n ; j++){
+      for(int k= j+1 ; k<n ; k++){
+        sum = arr[i]+arr[j]+arr[k];
+        if(sum>max && sum<=m)
+          max = sum;
+      }
+    }
+  }
+  printf("%d", max);
+  return 0;
+}
+  
