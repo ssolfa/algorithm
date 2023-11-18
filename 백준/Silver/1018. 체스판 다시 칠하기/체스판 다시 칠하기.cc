6b174fc7041dbc6b29052b992e933bc180a287e0
solmee <87332713+ssolfa@users.noreply.github.com>
2023-11-18 18:28:26 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string WB[8] =
+    {
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW"};
+
+string BW[8] =
+    {
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB"};
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    char str[N][M];
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cin >> str[i][j];
+        }
+    }
+
+    int result = 64;
+
+    for (int i = 0; i <= N - 8; i++)
+    {
+        for (int j = 0; j <= M - 8; j++)
+        {
+            int cnt_wb = 0, cnt_bw = 0;
+
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    if (str[i + x][j + y] != WB[x][y])
+                        cnt_wb++;
+
+                    if (str[i + x][j + y] != BW[x][y])
+                        cnt_bw++;
+                }
+            }
+
+            result = min(result, min(cnt_wb, cnt_bw));
+        }
+    }
+
+    cout << result;
+    return 0;
+}
