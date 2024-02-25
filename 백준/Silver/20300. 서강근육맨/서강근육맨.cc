4523895f58d486bdedef1c1a4728afc878c3a277
solmee <87332713+ssolfa@users.noreply.github.com>
2024-02-25 20:53:45 +0900

    [Silver III] Title: 서강근육맨, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/20300. 서강근육맨/서강근육맨.cc
+++ 백준/Silver/20300. 서강근육맨/서강근육맨.cc
@@ -16,14 +16,13 @@ int main() {
     sort(v.begin(), v.end());
 
     long long m = 0;
-
-    if(n%2){ // 홀수
-        m = v[n-1];
-        for(int i=0 ; i<(n-1)/2 ; i++){
+    if(n%2 != 0){
+        m = v.back();
+        for(int i=0 ; i<n/2 ; i++){
             m = max(m, v[i] + v[n - i - 2]);
         }
     }
-    else{ // 짝수
+    else{
         for(int i=0 ; i<n/2 ; i++){
             m = max(m, v[i] + v[n - i - 1]);
         }
