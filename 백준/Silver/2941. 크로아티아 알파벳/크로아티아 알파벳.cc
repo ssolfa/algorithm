4c3a79d1b1192b6b6762141104f892dc375d3d7b
solmee <87332713+ssolfa@users.noreply.github.com>
2025-06-03 21:54:53 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -4,19 +4,16 @@ using namespace std;
 
 int main()
 {
-    string croatian[8] = { "dz=", "c=", "c-", "d-", "lj", "nj", "s=", "z="};
+    string croatian[8] = { "dz=", "c=", "c-", "d-", "lj", "nj", "s=", "z=" };
     string str;
     cin >> str;
     
-    for(int i=0 ; i<8; i++){
-        while(str.find(croatian[i]) != string::npos) {
-            if(croatian[i] == "dz=") {
-                str.replace(str.find(croatian[i]), 3, "#");
-            } else {
-                str.replace(str.find(croatian[i]), 2, "#");
-            }
+    for (int i = 0; i < 8; i++) {
+        while (str.find(croatian[i]) != string::npos) {
+            str.replace(str.find(croatian[i]), croatian[i].length(), "#");
         }
     }
     
     cout << str.length();
+    return 0;
 }
