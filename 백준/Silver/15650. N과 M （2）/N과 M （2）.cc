041788a0565a9196f3e513af4095ce391d866aeb
solmee <87332713+ssolfa@users.noreply.github.com>
2025-06-24 22:13:28 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,31 +1,29 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int N, M;
-int arr[9];
-bool visited[9];
+int n,m;
+vector <int> result;
 
-void dfs(int k, int start){
-    if (k == M){
-        for (int i = 0; i < M; i++){
-            cout << arr[i]<< ' ';
+void dfs(int start, int depth){
+    if(depth == m){
+        for(int i=0 ; i<m ; i++){
+            cout << result[i] << ' ';
         }
         cout << '\n';
     }
-    else{
-        for (int i = start; i <= N; i++){
-            if (!visited[i]){
-                visited[i] = true;
-                arr[k] = i;
-                dfs(k + 1, i + 1);
-                visited[i] = false;
-            }
-        }
+    
+    for(int i=start ; i<=n ; i++){
+        result.push_back(i);
+        dfs(i+1, depth+1);
+        result.pop_back();
     }
 }
 
-int main(){
-    cin >> N >> M;
-    dfs(0, 1);
-    return 0;
+int main()
+{
+    cin >> n >> m;
+    
+    dfs(1, 0);
 }
